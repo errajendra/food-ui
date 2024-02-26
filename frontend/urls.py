353be27950eeeb614532bd87d5f1d9780b6c0a86
rajendraswooshelf <rajendras@wooshelf.com>
2024-02-26 14:09:08 +0530

    tc, about, calcellation, privacy policy pages

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,4 +5,9 @@ from .views import *
 urlpatterns = [
     path("", index, name="index"),
     path("ajax/", include('frontend.ajax.urls')),
+    
+    path("terms-and-conditions/", tc, name="tc"),
+    path("privacy-policy/", privacy_policy, name="privacy_policy"),
+    path("cancellation-policy/", cancelation_policy, name="cancelation_policy"),
+    path("about-us/", about_us, name="about_us"),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'frontend/index.html')
+
+
+# Terms & Conditions
+def tc(request):
+    return render(request, 'frontend/tc.html')
+
+
+# Privacy Policy
+def privacy_policy(request):
+    return render(request, 'frontend/privacy-policy.html')
+
+
+# Cancelation Policy
+def cancelation_policy(request):
+    return render(request, 'frontend/cancelation-policy.html')
+
+
+# About Us
+def about_us(request):
+    return render(request, 'frontend/about-us.html')
